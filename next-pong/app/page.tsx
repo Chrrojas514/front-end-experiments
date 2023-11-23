c6f75174469c1ec65d3e8fffca2200c897968caf
unknown <chrisrojas514@gmail.com>
2023-11-23 18:11:41 -0500

    deployment change from localhost to new domain

--- next-pong/app/page.tsx
+++ next-pong/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <a href='http://localhost:3000/gameRooms'>cool pong site</a>
+      <a href='https://https://pong-ten-phi.vercel.app/gameRooms'>cool pong site</a>
       <div></div>
     </main>
   )
