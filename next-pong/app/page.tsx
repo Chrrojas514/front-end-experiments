d91e5334fac8d0e0116f1eb4ad65eb889bc7e59c
unknown <chrisrojas514@gmail.com>
2023-11-29 14:29:03 -0500

    reverting previous change to href on initial page

--- next-pong/app/page.tsx
+++ next-pong/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <a href='https://https://pong-ten-phi.vercel.app/gameRooms'>cool pong site</a>
+      <a href='http://localhost:3000/gameRooms'>cool pong site</a>
       <div></div>
     </main>
   )
