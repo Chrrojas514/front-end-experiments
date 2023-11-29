15af2a25738ea9dd9fdb0a021d84e0411e18edf3
unknown <chrisrojas514@gmail.com>
2023-11-29 14:35:00 -0500

    fix for deployment

--- next-pong/app/page.tsx
+++ next-pong/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <a href='http://localhost:3000/gameRooms'>cool pong site</a>
+      <a href='https://pong-git-main-chris-projects-10429c46.vercel.app/gameRooms'>cool pong site</a>
       <div></div>
     </main>
   )
