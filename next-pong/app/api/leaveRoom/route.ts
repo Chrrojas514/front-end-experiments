302ba1e1f5fc38401d46b9a519641c9c9492b460
unknown <chrisrojas514@gmail.com>
2023-12-03 17:10:54 -0500

    migrated leaveRoom endpoint

--- next-pong/app/api/leaveRoom/route.ts
+++ next-pong/app/api/leaveRoom/route.ts
@@ -1,6 +1,45 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 import prisma from "@/prisma/client"
 
-export async function POST() {
+export async function POST(request: Request) {
+    const data = await request.json()
   
-}
+    const target = await prisma.gameState.findUnique({
+      where: {
+        roomId: data.roomId
+      }
+    });
+  
+    if (!target) {
+      return NextResponse.json('Room not found', {status: 404})
+    }
+  
+    // If player that requested to leave is A, reset player name to empty string
+    if (data.playerName === target.playerA) {
+      const updatedTarget = await prisma.gameState.update({
+        where: {
+          roomId: data.roomId,
+        },
+        data: {
+          playerA: "",
+        }
+      });
+  
+      return NextResponse.json(updatedTarget, {status: 201});
+    }
+  
+    if (data.playerName === target.playerB) {
+      const updatedTarget = await prisma.gameState.update({
+        where: {
+          roomId: data.roomId,
+        },
+        data: {
+          playerB: "",
+        }
+      });
+  
+      return NextResponse.json(updatedTarget, {status: 201});
+    }
+  
+    return NextResponse.json('Room is full', {status: 400})
+  }
