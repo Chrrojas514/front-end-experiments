c7ca9c4af16d8831bc92750560dd26a3833549e9
unknown <chrisrojas514@gmail.com>
2023-12-03 17:05:00 -0500

    migrated joinRoom endpoint, adjusted more links in frontend from port 5000 to 3000

---
+++ next-pong/app/api/joinRoom/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server"
+import prisma from "@/prisma/client"
+
+export async function POST(request: Request) {
+  const data = await request.json()
+
+  const target = await prisma.gameState.findUnique({
+    where: {
+      roomId: data.roomId
+    }
+  });
+
+  if (!target) {
+    return NextResponse.json('Room not found', {status: 404})
+  }
+
+  // If player A has no current name, assign it to A
+  if (!target.playerA) {
+    const updatedTarget = await prisma.gameState.update({
+      where: {
+        roomId: data.roomId,
+      },
+      data: {
+        playerA: data.playerName
+      }
+    });
+
+    return NextResponse.json(updatedTarget, {status: 201});
+  }
+
+  if (!target.playerB) {
+    const updatedTarget = await prisma.gameState.update({
+      where: {
+        roomId: data.roomId,
+      },
+      data: {
+        playerB: data.playerName
+      }
+    });
+
+    return NextResponse.json(updatedTarget, {status: 201});
+  }
+
+  return NextResponse.json('Room is full', {status: 400})
+}
--- next-pong/app/gameRooms/play/[roomId]/[playerName]/page.tsx
+++ next-pong/app/gameRooms/play/[roomId]/[playerName]/page.tsx
@@ -14,7 +14,7 @@ function GameRoom() {
   const [refetchInterval, setRefetchInterval] = useState(200)
 
   const gameStateQuery = useQuery<GameState>('gameState', () =>
-  fetch(`http://localhost:5000/gameStates/${params.roomId}`).then(res =>
+  fetch(`http://localhost:3000/api/gameStates/${params.roomId}`).then(res =>
     res.json()), {refetchInterval, onError:() => setRefetchInterval(0)}
     )
   
